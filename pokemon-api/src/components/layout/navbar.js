60f8d8f89a6d2d74394f7f3045cea050eb3f3d2f
shaneogrady <shaneogrady1991@gmail.com>
2019-02-03 21:14:13 +0000

    initial components and styles

---
+++ pokemon-api/src/components/layout/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class navbar extends Component {
+	render() {
+		return (
+			<div>
+				<nav className="navbar navbar-expand-md navbar-dark fixed-top">
+					<a href="test.html">Shane</a>
+				</nav>
+			</div>
+		)
+	}
+}
