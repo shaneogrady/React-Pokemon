6f0d8f0e9e3206648613c8ea80313a3f9f94eb6c
shaneogrady <shaneogrady1991@gmail.com>
2019-02-07 23:46:07 +0000

    pulling pokemon images from github repo based on id

--- src/components/pokemon/pokemonCard.js
+++ src/components/pokemon/pokemonCard.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 
+import styled from 'styled-components';
+
+const Sprite = styled.img`
+    width: 5em;
+    height: 5em;
+`;
+
 export default class PokemonCard extends Component {
 
     state = {
@@ -27,6 +34,9 @@ export default class PokemonCard extends Component {
             <div className="col-md-3 col-sm-6 mb-5">
                 <div className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                    <Sprite className="card-img-top rounded mx-auto mt-2" src={this.state.imageURL}>
+
+                    </Sprite>
                     <div className="card-body mx-auto">
                         <h6 className="card-title">{this.state.name}</h6>
                     </div>
