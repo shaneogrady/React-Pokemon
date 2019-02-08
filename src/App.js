6d17edf5ba8305ff0791448b459a20ee9edd6dff
shaneogrady <shaneogrady1991@gmail.com>
2019-02-08 10:36:28 +0000

    update styles and add router-dom to link to seperate pages based on id

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import NavBar from './components/layout/navbar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './components/layout/dashboard';
+import Pokemon from './components/pokemon/Pokemon';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <div className="container">
-          <Dashboard />
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+              <Dashboard />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/pokemon/pokemonCard.js
+++ src/components/pokemon/pokemonCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 import styled from 'styled-components';
 
@@ -15,6 +16,22 @@ const Card = styled.div`
     transition: all 0.3s cubic-bezier(0.25, 0.8, .25, 1 );
     &:hover {
         box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+    }
+    -o-user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none !important;
+    color: black !important;
+    &:focus,
+    &:hover,
+    &:visible,
+    &:link,
+    &:active {
+        text-decoration: none;
     }
 `;
 
@@ -44,50 +61,52 @@ export default class PokemonCard extends Component {
 
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <Card className="card">
-                    <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+                    <Card className="card">
+                        <h5 className="card-header">{this.state.pokemonIndex}</h5>
 
-                    {this.state.imageLoading ? (
-                        <img
-                            src={spinner}
-                            style={{ width: '5em', height: '5em' }}
-                            className="card-img-top rounded mx-auto d-block mt-2"
-                        />
-                    ) : null}
+                        {this.state.imageLoading ? (
+                            <img
+                                src={spinner}
+                                style={{ width: '5em', height: '5em' }}
+                                className="card-img-top rounded mx-auto d-block mt-2"
+                            />
+                        ) : null}
 
-                    <Sprite
-                        className="card-img-top rounded mx-auto mt-2"
-                        onLoad={() => this.setState({ imageLoading: false })}
-                        onError={() => this.setState({ tooManyRequests: true })}
-                        src={this.state.imageURL}
-                        style={
-                            this.state.tooManyRequests ? { display: "none" } :
-                                this.state.imageLoading ? null : { display: "block" }
-                        }
-                    />
-
-                    {this.state.tooManyRequests ? (
-                        <h6 className="mx-auto">
-                            <span className="badge badge-danger mt-2">
-                                Too Many Requests
-                            </span>
-                        </h6>
-                    ) : null}
-
-                    <div className="card-body mx-auto">
-                        <h6 className="card-title">
-                            {
-                                this.state.name
-                                    .toLowerCase()
-                                    .split(' ')
-                                    .map(
-                                        letter => letter.charAt(0).toUpperCase() + letter.substring(1)
-                                    )
-                                    .join('')
+                        <Sprite
+                            className="card-img-top rounded mx-auto mt-2"
+                            onLoad={() => this.setState({ imageLoading: false })}
+                            onError={() => this.setState({ tooManyRequests: true })}
+                            src={this.state.imageURL}
+                            style={
+                                this.state.tooManyRequests ? { display: "none" } :
+                                    this.state.imageLoading ? null : { display: "block" }
                             }
-                        </h6>
-                    </div>
-                </Card>
+                        />
+
+                        {this.state.tooManyRequests ? (
+                            <h6 className="mx-auto">
+                                <span className="badge badge-danger mt-2">
+                                    Too Many Requests
+                            </span>
+                            </h6>
+                        ) : null}
+
+                        <div className="card-body mx-auto">
+                            <h6 className="card-title">
+                                {
+                                    this.state.name
+                                        .toLowerCase()
+                                        .split(' ')
+                                        .map(
+                                            letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                                        )
+                                        .join('')
+                                }
+                            </h6>
+                        </div>
+                    </Card>
+                </StyledLink>
             </div>
         )
     }
