c8d669b2e827cc73e1e4eb8afa0e0f3a5201fb06
shaneogrady <shaneogrady1991@gmail.com>
2019-02-08 01:03:49 +0000

    hover styles

--- src/components/pokemon/pokemonCard.js
+++ src/components/pokemon/pokemonCard.js
@@ -10,6 +10,14 @@ const Sprite = styled.img`
     display: none;
 `;
 
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, .25, 1 );
+    &:hover {
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+    }
+`;
+
 export default class PokemonCard extends Component {
 
     state = {
@@ -36,7 +44,7 @@ export default class PokemonCard extends Component {
 
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
+                <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
 
                     {this.state.imageLoading ? (
@@ -79,7 +87,7 @@ export default class PokemonCard extends Component {
                             }
                         </h6>
                     </div>
-                </div>
+                </Card>
             </div>
         )
     }
